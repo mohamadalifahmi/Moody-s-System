b45ccdda026d760ebe894e91301a9ee19c3cc76d
Mohamad Ali Fahmi <mf627137mohamad@gmail.com>
2026-06-01 21:22:20 +0300

    fix start.sh: use sh instead of bash, consolidate db:seed

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 php artisan optimize:clear
 php artisan migrate --force
-php artisan db:seed --class=TenantSeeder --force
-php artisan db:seed --class=UserSeeder --force
-php artisan db:seed --class=ExpenseCategorySeeder --force
-php artisan db:seed --class=ProductCategorySeeder --force
-php artisan db:seed --class=ProductSeeder --force
-php artisan db:seed --class=SupplierSeeder --force
+php artisan db:seed --force
 php artisan serve --host=0.0.0.0 --port=$PORT
